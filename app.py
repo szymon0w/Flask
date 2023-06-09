973e5c4df2737aaca368803de4d2cd353a1d29f9
szymon0w <szymfre33@gmail.com>
2023-06-09 17:05:32 +0200

    commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
